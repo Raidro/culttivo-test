fb5bd31f1dc9373410e97380d3cca1c17f1901f5
Mateus Sátiro <mateus_sat@hotmail.com>
2024-11-18 12:22:03 -0300

    feat: add o node_modules

--- marvel-api/src/App.jsx
+++ marvel-api/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   const [page, setPage] = useState(0);
   const [offSet, setOffset] = useState(1);
   const [total, setTotal] = useState(0);
+
   useEffect(() => {
     async function fetchListCharacts() {
       const infos = await getAllCharacters({ offSet: offSet });
@@ -18,6 +19,8 @@ function App() {
     }
     fetchListCharacts();
   }, [offSet, setOffset]);
+
+  console.log(infos);
 
   return (
     characterList && (
